2ef966574ac6dbc011c90c4e536feb2c81df104b
Egor18032019 <goro4@mail.ru>
2023-09-27 10:11:48 +0500

    js

--- src/test/java/pages/PracticeForm.java
+++ src/test/java/pages/PracticeForm.java
@@ -288,8 +288,9 @@ public class PracticeForm extends PageBase {
     }
 
     private void scrollWithJavaScript(WebElement element) {
-        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true); window.scrollBy(0, -window.innerHeight / 4);", element);
-//        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView();", element);
+//        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true); window.scrollBy(0, -window.innerHeight / 4);", element);
+//        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(false);", element);
 //        js.executeScript("window.scrollTo(0, Math.max(document.documentElement.scrollHeight, document.body.scrollHeight, document.documentElement.clientHeight));")
 //        Actions actions = new Actions(webDriver);
 //        actions.moveToElement(element);
